5c430664e299a20dadfd845e245170aeaeb473af
Татьяна Симченко <misstaty8181@gmail.com>
2022-09-27 13:06:11 +0200

    add about and experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
-// import "./about.css"
-
+import React from "react";
+import ME2 from "../../assets/me2.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+import "./about.css";
 
 const About = () => {
   return (
-    <section id='about'>
-      About
-    </section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME2} alt="AboutImage" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experiense</h5>
+              <small>Just started</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>Just started</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+ Pet projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum ad
+            ipsa dolorum aliquam quo nulla minima expedita, ducimus provident
+            non quos sunt, facilis ipsam ea architecto maiores quasi officia
+            quisquam.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,96 @@
-import React from 'react';
-// import "./experience/css"
+import React from "react";
+import { BsPatchCheckFill } from "react-icons/bs";
+import "./experience.css";
 
 const Experience = () => {
   return (
     <section id="experience">
-      Experience
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__design">
+          <h3>Design Experiense</h3>
+          <div className="experience__content">
+            <article className="experience__ditails">
+              <BsPatchCheckFill className="experience__ditails-icon"/>
+              <div>
+                <h4>Figma</h4>
+                <small className="text-light">Experiesed</small>
+              </div>
+            </article>
+            <article className="experience__ditails">
+              <BsPatchCheckFill className="experience__ditails-icon"/>
+              <div>
+                <h4>Photoshop</h4>
+                <small className="text-light">Experiesed</small>
+              </div>
+            </article>
+            <article className="experience__ditails">
+              <BsPatchCheckFill className="experience__ditails-icon"/>
+              <div>
+                <h4>Illustrator</h4>
+                <small className="text-light">Experiesed</small>
+              </div>
+            </article>
+            <article className="experience__ditails">
+              <BsPatchCheckFill className="experience__ditails-icon"/>
+              <div>
+                <h4>Prototyping</h4>
+                <small className="text-light">Experiesed</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__ditails">
+              <BsPatchCheckFill className="experience__ditails-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experiesed</small>
+              </div>
+            </article>
+            <article className="experience__ditails">
+              <BsPatchCheckFill className="experience__ditails-icon"/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experiesed</small>
+              </div>
+            </article>
+            <article className="experience__ditails">
+              <BsPatchCheckFill className="experience__ditails-icon"/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experiesed</small>
+              </div>
+            </article>
+            <article className="experience__ditails">
+              <BsPatchCheckFill className="experience__ditails-icon"/>
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experiesed</small>
+              </div>
+            </article>
+            <article className="experience__ditails">
+              <BsPatchCheckFill className="experience__ditails-icon"/>
+              <div>
+                <h4>Git</h4>
+                <small className="text-light">Experiesed</small>
+              </div>
+            </article>
+            <article className="experience__ditails">
+              <BsPatchCheckFill className="experience__ditails-icon"/>
+              <div>
+                <h4>SCSS</h4>
+                <small className="text-light">Experiesed</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
